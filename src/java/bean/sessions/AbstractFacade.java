2d0339681b81d2eefae1ea41027bfb1aaca0fb2f
Juan David Gomez Forero <fogodaju@gmail.com>
2020-02-22 12:10:19 -0500

    Login Finalizado

--- src/java/bean/sessions/AbstractFacade.java
+++ src/java/bean/sessions/AbstractFacade.java
@@ -61,4 +61,15 @@ public abstract class AbstractFacade<T> {
         return ((Long) q.getSingleResult()).intValue();
     }
     
+    public T login(String userName, String userPassword){
+        javax.persistence.criteria.CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+
+        javax.persistence.criteria.CriteriaQuery<T> cq = cb.createQuery(entityClass);
+        javax.persistence.criteria.Root<T> c = cq.from(entityClass);
+        cq.select(c).where(cb.equal(c.get("userName"),userName),cb.equal(c.get("userPassword"),userPassword));
+
+        //javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        //cq.select(cq.from(entityClass));
+        return (T) getEntityManager().createQuery(cq).getSingleResult();
+    }
 }
